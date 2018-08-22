32ee22a0bca6cecb33ef49118150604d7947f48d
passingthru67 <passingthru67@gmail.com>
2018-08-22 14:22:29 -0400

    Use localeCompare when sorting applications
    NOTE:
    Otherwise the accented letters are sort after all others letters.
    For example ..
    1-Normal sort : E É Z
    1-Gnomenu sort : E Z É
    2- Normal sort : De Dé Du
    2- Gnomenu sort : De Du Dé

--- extension.js
+++ extension.js
@@ -1464,7 +1464,7 @@ const PanelMenuButton = new Lang.Class({
         // Ignore favorites when sorting
         if (category_menu_id != 'favorites') {
             res.sort(function(a,b) {
-                return a.get_name().toLowerCase() > b.get_name().toLowerCase();
+                return a.get_name().toLowerCase().localeCompare(b.get_name().toLowerCase());
             });
         }
 
